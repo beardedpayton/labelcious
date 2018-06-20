714a7a6133da480613347f84b9b4001f51816317
beardedpayton <beardedpayton@gmail.com>
2018-06-20 08:20:56 -0400

    fixed assigment to constant

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = robot => {
   robot.on('issues.opened', async context => {
-    config = await context.config('config.yml')
+    const config = await context.config('config.yml')
     const params = context.issue({body: config.goodFirstIssueComment})
     return context.github.issues.createComment(params)
   })
