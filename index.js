d220d114477c67d41c1c6026187baa3d142e265f
beardedpayton <beardedpayton@gmail.com>
2018-06-21 11:44:30 -0400

    refactor native github label code for new data structure

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 async function labelcious(context) {
-  const config = await context.config('config.yml')
-    
-  // grab payload label
+  const config = await context.config('config.yml') 
   const label = context.payload.label
 
-  // do nothing if no labels exists
   if (!label) {
     return
   }
 
-  // check for label and comment on issue if label exists
   if (label) {
 
     if (config.customLabels === true) {
 
-     // set custom comment for each dynamic variable
      for (customLabel in config.labels) {
         if (label.name === customLabel) {
           const params = context.issue({body: config.labels[customLabel]})
@@ -23,46 +18,12 @@ async function labelcious(context) {
      }
     }
 
-    if (label.name === 'bug' && config.bugComment != false) {
-      const params = context.issue({body: config.bugComment})
-      return context.github.issues.createComment(params)
+    for(nativeLabel in config.githubLabels) {
+      if (label.name === nativeLabel) {
+        const params = context.issue({body: config.githubLabels[nativeLabel]})
+        context.github.issues.createComment(params)
+      }
     }
-
-    if (label.name === 'duplicate' && config.duplicateComment != false) {
-      const params = context.issue({body: config.duplicateComment})
-      return context.github.issues.createComment(params)
-    }
-
-    if (label.name === 'enhancement' && config.enhancementComment != false) {
-      const params = context.issue({body: config.enhancementComment})
-      return context.github.issues.createComment(params)
-    }
-
-    if (label.name === 'good first issue' && config.goodFirstIssueComment != false) {
-      const params = context.issue({body: config.goodFirstIssueComment})
-      return context.github.issues.createComment(params)
-    }
-
-    if (label.name === 'help wanted' && config.helpWantedComment != false) {
-      const params = context.issue({body: config.helpWantedComment})
-      return context.github.issues.createComment(params)
-    }
-
-    if (label.name === 'invalid' && config.invalidComment != false) {
-      const params = context.issue({body: config.invalidComment})
-      return context.github.issues.createComment(params)
-    }
-
-    if (label.name === 'question' && config.questionComment != false) {
-      const params = context.issue({body: config.questionComment})
-      return context.github.issues.createComment(params)
-    }
-
-    if (label.name === 'wont fix' && config.wontFixComment != false) {
-      const params = context.issue({body: config.wontFixComment})
-      return context.github.issues.createComment(params)
-    }
-
   }
 }
 
