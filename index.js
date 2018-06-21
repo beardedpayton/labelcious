71c22a493966a1e755611db48ab776e60896f1b4
beardedpayton <beardedpayton@gmail.com>
2018-06-21 08:21:03 -0400

    native github label check and comment feat

--- index.js
+++ index.js
@@ -1,8 +1,61 @@
-module.exports = robot => {
-  robot.on('issues.opened', async context => {
-    const config = await context.config('config.yml')
-    const params = context.issue({body: config.goodFirstIssueComment})
-    return context.github.issues.createComment(params)
-  })
+async function labelcious(context) {
+  const config = await context.config('config.yml')
+    
+  // grab payload label
+  const label = context.payload.label
+
+  // do nothing if no labels exists
+  if (!label) {
+    return
+  }
+
+  // check for label and comment on issue if label exists
+  if (label) {
+
+    if (label.name === 'bug' && config.bugComment != false) {
+      const params = context.issue({body: config.bugComment})
+      return context.github.issues.createComment(params)
+    }
+
+    if (label.name === 'duplicate' && config.duplicateComment != false) {
+      const params = context.issue({body: config.duplicateComment})
+      return context.github.issues.createComment(params)
+    }
+
+    if (label.name === 'enhancement' && config.enhancementComment != false) {
+      const params = context.issue({body: config.enhancementComment})
+      return context.github.issues.createComment(params)
+    }
+
+    if (label.name === 'good first issue' && config.goodFirstIssueComment != false) {
+      const params = context.issue({body: config.goodFirstIssueComment})
+      return context.github.issues.createComment(params)
+    }
+
+    if (label.name === 'help wanted' && config.helpWantedComment != false) {
+      const params = context.issue({body: config.helpWantedComment})
+      return context.github.issues.createComment(params)
+    }
+
+    if (label.name === 'invalid' && config.invalidComment != false) {
+      const params = context.issue({body: config.invalidComment})
+      return context.github.issues.createComment(params)
+    }
+
+    if (label.name === 'question' && config.questionComment != false) {
+      const params = context.issue({body: config.questionComment})
+      return context.github.issues.createComment(params)
+    }
+
+    if (label.name === 'wont fix' && config.wontFixComment != false) {
+      const params = context.issue({body: config.wontFixComment})
+      return context.github.issues.createComment(params)
+    }
+
+  }
 }
- 
+
+module.exports = robot => {
+  robot.on('issues.opened', labelcious),
+  robot.on('issues.labeled', labelcious)
+}
